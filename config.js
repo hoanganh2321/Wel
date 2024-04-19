c55cb7f98107d8003529a162b5507280d5924588
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-19 16:59:42 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,32 @@
+
+
+module.exports = {
+  TOKEN: "MTIyNTQyOTY0Njg1NDU5MDUwNg.Gyzg_d.VDJIAkiobGO6DpA6ZlXt1mpYscSRvsnq4Wi2ps",
+  ownerID: ["779507251282968587", ""], 
+  commandsDir: './commands',  
+  embedColor: "00fbff",
+  errorLog: "", 
+
+}
+
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
+
